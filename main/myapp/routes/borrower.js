81ce71d967ca6cff2b72668ac8c0e22669fdd93a
whoopeedeedoo <whoopeedeedoo@gmail.com>
2015-05-25 20:10:22 +0800

    add borrower pages

--- main/myapp/routes/borrower.js
+++ main/myapp/routes/borrower.js
@@ -1,41 +1,84 @@
-var library=require( './library.js' );
+var library = require('./library.js');
 var mongoose = require('mongoose');
+var Borrows = mongoose.model('Borrows');
 
 var express = require('express');
 var router = express.Router();
 
 // U can try this by /borrower
-router.get('/',library.newMsgChecker, function (req, res) {
-	var auRst=null;
-	if(req.isAuthenticated()){
-		auRst=req.user.Username;
+router.get('/', library.newMsgChecker, function(req, res) {
+	var auRst = null;
+	if (req.isAuthenticated()) {
+		auRst = req.user.Username;
 	}
-	
+
 	//get data from database and process them here
-	
+
 	//pass what u get from database and send them into ejs in this line
-	res.render('ejsExample',{newlrmNum:req.newlrmNumber,newlsmNum:req.newlsmNumber,userName:auRst});
+	res.render('ejsExample', {
+		newlrmNum: req.newlrmNumber,
+		newlsmNum: req.newlsmNumber,
+		userName: auRst
+	});
 });
 
 // this is the basic type when page no need to ensure authenticated. U can try this by /borrower/borrowerExample
-router.get('/borrowerExample',library.newMsgChecker, function (req, res) {
-	var auRst=null;
-	if(req.isAuthenticated()){
-		auRst=req.user.Username;
+router.get('/borrowPage', library.ensureAuthenticated, library.newMsgChecker, function(req, res) {
+	var auRst = null;
+	if (req.isAuthenticated()) {
+		auRst = req.user.Username;
 	}
-	
+
+	//console.log(req.user);
 	//get data from database and process them here
-	
+
 	//pass what u get from database and send them into ejs in this line
-	res.render('ejsExample',{newlrmNum:req.newlrmNumber,newlsmNum:req.newlsmNumber,userName:auRst});
+	res.render('borrowPage', {
+		createdBy: req.user._id
+	});
 });
 
 // this is the basic type when page need to ensure authenticated. U can try this by /borrower/borrowerExample2
-router.get('/borrowerExample2',library.ensureAuthenticated,library.newMsgChecker, function (req, res) {
+router.get('/checkMatch', library.ensureAuthenticated, library.newMsgChecker, function(req, res) {
 	//get data from database and process them here
-	
+
 	//pass what u get from database and send them into ejs in this line
-	res.render('ejsExample',{newlrmNum:req.newlrmNumber,newlsmNum:req.newlsmNumber,userName:req.user.Username});
+	res.render('checkMatch', {
+
+	});
 });
 
+// this is the basic type when page no need to ensure authenticated. U can try this by /borrower/borrowerExample
+router.get('/borrowerPanel', library.newMsgChecker, function(req, res) {
+	var auRst = null;
+	if (req.isAuthenticated()) {
+		auRst = req.user.Username;
+	}
+
+	//get data from database and process them here
+
+	//pass what u get from database and send them into ejs in this line
+	res.render('borrowerPanel', {
+		newlrmNum: req.newlrmNumber,
+		newlsmNum: req.newlsmNumber,
+		userName: auRst
+	});
+});
+
+// this is the basic type when page no need to ensure authenticated. U can try this by /borrower/borrowerExample
+router.get('/borrowerConfirmedMatch', library.newMsgChecker, function(req, res) {
+	var auRst = null;
+	if (req.isAuthenticated()) {
+		auRst = req.user.Username;
+	}
+
+	//get data from database and process them here
+
+	//pass what u get from database and send them into ejs in this line
+	res.render('borrowerConfirmedMatch', {
+		newlrmNum: req.newlrmNumber,
+		newlsmNum: req.newlsmNumber,
+		userName: auRst
+	});
+});
 module.exports = router;
